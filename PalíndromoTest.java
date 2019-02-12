26ed9006c55dcf7e932ee8e959f81e388a5fb598
Sandra90-16 <sandracabrera90@gmail.com>
2019-02-12 09:57:13 +0100

    cambio 10

---
+++ PalíndromoTest.java
@@ -0,0 +1,45 @@
+import junit.framework.TestCase;
+
+public class PalíndromoTest extends TestCase {
+
+	public PalíndromoTest(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+	
+
+	 public void testEspalindromoVacio() {
+		 Palindromo palindromo = new Palindromo();
+	        assertTrue(Palindromo.funcion(" "));
+	
+	       
+
+	    }
+	 public void testNoEspalindromoImpar() {
+		 Palindromo palindromo = new Palindromo();
+		 assertFalse(Palindromo.funcion("lkjhg"));
+		
+	    }
+	 public void testEspalindromoImpar() {
+		 Palindromo palindromo = new Palindromo();
+		 assertTrue(Palindromo.funcion("ana"));
+		
+	    }
+	 public void testEspalindromoPar() {
+		 Palindromo palindromo = new Palindromo();
+		 assertTrue(Palindromo.funcion("123321"));
+		
+	    }
+	 public void testNoEspalindromoPar() {
+		 Palindromo palindromo = new Palindromo();
+		 assertFalse(Palindromo.funcion("12345678"));
+		
+	    }
+	}
