53124bf59d21ae2346786d0a23a088bd09f4ee89
Sandra90-16 <sandracabrera90@gmail.com>
2019-02-12 09:58:41 +0100

    subida clase palidnromo

---
+++ Palindromo.java
@@ -0,0 +1,43 @@
+public class Palindromo {
+
+	public static boolean funcion(String a) {
+
+		boolean res = true;
+
+		int i = 0; //1
+
+		if(a.length() % 2 == 0) { //2
+
+			while(i<a.length()/2 && res) { //3 y //4
+
+				res = a.charAt(i) == a.charAt((a.length()-1)-i); //5
+
+				i++;
+
+			}
+
+		}
+
+		else {
+
+			while(i<(a.length()/2)+1 && res) { //6
+
+				res = a.charAt(i) == a.charAt((a.length()-1)-i); //7
+
+				i++;
+
+			}
+
+		}
+
+		return res;//8
+
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Es un palindromo " + funcion("dabale arroz a la zorra el abad"));
+	}
+
+}
+
+
